b950f167ac2ec5f2242405608fc7bb4b8ef39441
Krisna Renaldi <krisna.renaldi@gmail.com>
2026-09-11 16:59:22 +0700

    update docker compose v3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -d /uploads ]; then
-  chown -R appuser:appuser /uploads
+  chown -R appuser:appuser /uploads 2>/dev/null || chmod -R 777 /uploads 2>/dev/null || true
 fi
 
 exec gosu appuser "$@"
